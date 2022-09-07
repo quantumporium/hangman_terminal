094382086ba30906dc73bcf8fd528d8a6ce2baba
quantumporium <quantumporium@email.com>
2022-09-07 10:29:55 -0700

    final version of the software

--- src/libs.py
+++ src/libs.py
@@ -4,7 +4,11 @@ from random import randint as random_nb
 #
 
 def info_header():
-    print("Version: 0.1\nWelcome to hangmane (the terminal version)\nHere are the rule\n[+] You have 5 chances to get the hidden word right\n[+] You can either guess a entire word or a single character\n\n\t\t-=-=-=-=-= Good Luck =-=-=-=-=-");
+    print("Version: 0.1\nWelcome to hangman (the terminal version)\nHere are the rule\n[+] You have 5 chances to get the hidden word right\n[+] You can either guess a entire word or a single character\n\t\t-=-=-=-=-= Good Luck =-=-=-=-=-\n\n");
+
+def fatal():
+    print("\n[FATAL] a fatal error occur.\tThe program will close shortly")
+    exit()
 
 def choose_random_words(FILE_PATH):
     with open(FILE_PATH, 'r') as file:
@@ -13,7 +17,7 @@ def choose_random_words(FILE_PATH):
     return
 
 def show_game_stat(guess_left, list_wrong_letter):
-    print(f"You have {guess_left} guesses left\t\tWrong letter: {list_wrong_letter}")
+    print(f"\nYou have {guess_left} guesses left\t\tWrong letter: {list_wrong_letter}")
     return
 
 # to implement
@@ -32,37 +36,65 @@ def show_hidden_progress(right_char, hidden_word, hidden_progress):
 
 
 def get_user_guess():
-    user_guess = input("\nenter your guess =>  ")
+    user_guess = input("\n[+] Enter your guess =>  ")
     if len(user_guess) == 1:
         # enter a character
         return ('char', user_guess.lower()) # all the answer are lowercase
     else:
-        return ('word', user_guess.lower()) # all the answer are lowercase
+        return ('word', user_guess.strip().lower()) # change this # all the answer are lowercase
         # enter a words
 
 # might need to change it
 def parse_guess_letter(user_guess, hidden_word, list_wrong_letter):
     if user_guess[1] not in hidden_word:
         list_wrong_letter.append( user_guess[1] )
-        print(f"The letter {user_guess[1]} is not in the hidden words")
+        print(f"[B] The letter {user_guess[1]} is not in the hidden words")
         return ('wrong_char')
     
     else:
-        print(f"The letter {user_guess[1]} is in the hidden words")
+        print(f"[G] The letter {user_guess[1]} is in the hidden words")
         return ('right_char', user_guess)
 
     return
 
+## <-- focus attention here
+
 def parse_guess_words(user_guess, hidden_word):
-    if user_guess[1] == hidden_word:
-        return True
+    # variable used
+    guess = user_guess[1].strip()
+    hidden = hidden_word.strip()
+
+    if guess == hidden: # change this
+        return ('good_word')
     
-    if user_guess[1] != hidden_word:
-        print(f"{ hidden_word } isn't the word, try again")
+    if guess != hidden:
         return ('wrong_word')
 
-def no_guess_left():
-    print("\n\n\t\t=-=-=-=-=- Darn it, you failed you have no guesses left -=-=-=-=-=")
+
+def debug_guess_words(user_guess, hidden_word):
+    print("\n\n-=-=-=-=-= Begining debug mode =-=-=-=-=-=")
+    
+    print("[+] the expression is: hidden_word != user_guess[1] \n\n")
+
+    print(f"[+] the user enter: { user_guess[1] }")
+    print(f"[+] the datatype of the user is: { type(user_guess[1]) }\n")
+    print(f"[+] the hidden_word was: { hidden_word}")
+    print(f"[+] the hidden_word datatype was: { type(hidden_word) }\n")
+    
+    print(f"[+] bool value of hidden_word == user_guess: { hidden_word.strip() == user_guess[1].strip()}")
+    
+    print("-=-=-=-=-= End of debug mode =-=-=-=-=-=-\n\n")
+
+## 
+
+def no_guess_left(hidden_word):
+    print(f"\n\n\t=-=-=-=-=- You have no guesses left, the hidden word was: {hidden_word.strip().upper()} -=-=-=-=-=")
+
+def bad_word():
+    print("\n\n\t\t=-=-=-=-=- Darn it, this isn't the good word -=-=-=-=-=")
 
 def good_word():
     print("\n\n\t\t=-=-=-=-=- Congratulation you found the hidden word -=-=-=-=-=")
+
+def show_hidden_word(hidden_word):
+    print(f"\n\n\t\t=-=-=-=-=- The hidden word was { hidden_word.upper().strip() } -=-=-=-=-=")
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from libs import *
 FILE_PATH = "database/words_database.txt"
 HIDDEN_WORD = None 
 HIDDEN_PROGRESS = None
-AMMOUNT_GUESS = 5 # You can change this to your licking
+AMMOUNT_GUESS = 10 # You can change this to your licking
 WRONG_CHAR = []
 ROUND_RIGHT_CHAR = None
 
@@ -15,37 +15,39 @@ if __name__ == "__main__":
     info_header()
     
     HIDDEN_WORD = choose_random_words(FILE_PATH).lower() # make hidden_word lower
-    
+   
     HIDDEN_PROGRESS = create_hidden_word_progress(HIDDEN_WORD)
+    try:
+        for i in range(AMMOUNT_GUESS + 1):
+            show_game_stat( AMMOUNT_GUESS - i, WRONG_CHAR)
 
-    for i in range(AMMOUNT_GUESS + 1):
-        show_game_stat( AMMOUNT_GUESS - i, WRONG_CHAR)
-
-        show_hidden_progress(ROUND_RIGHT_CHAR ,HIDDEN_WORD, HIDDEN_PROGRESS)
+            show_hidden_progress(ROUND_RIGHT_CHAR ,HIDDEN_WORD, HIDDEN_PROGRESS)
         
-        user_input = get_user_guess()
+            if (AMMOUNT_GUESS - i ) == 0:
+                print("\n\t[ATTENTION] You have 1 guess left, use it wisely".upper())
 
-        print(f"\n\t[HIDDEN WORD] => {HIDDEN_WORD}\n")
+            user_input = get_user_guess()
 
-        if user_input[0] == 'char':
-            check_char = parse_guess_letter(user_input, HIDDEN_WORD, WRONG_CHAR)
+            if user_input[0] == 'char':
+                check_char = parse_guess_letter(user_input, HIDDEN_WORD, WRONG_CHAR)
 
-            if check_char[0] == 'right_char':
-                ROUND_RIGHT_CHAR = check_char[1][1]
+                if check_char[0] == 'right_char':
+                    ROUND_RIGHT_CHAR = check_char[1][1]
         
-
-        if user_input[0] == 'word':
-            good_word = parse_guess_words(user_input, HIDDEN_WORD)
+            if user_input[0] == 'word':
+                word_to_check = parse_guess_words(user_input, HIDDEN_WORD)
             
-            if good_word == True:
-                print("congratulation") # replace by something else
-                exit()
-        
+                if word_to_check == 'good_word':
+                    good_word() # replace by something else
+                    show_hidden_word(HIDDEN_WORD) 
+                    exit()
 
-        if (AMMOUNT_GUESS - i ) == 1:
-            print("\n\t[ATTENTION] You have 1 guess left, use it wisely\n".upper())
+                if word_to_check == 'wrong_word':
+                    if not ((AMMOUNT_GUESS - i) == 0):
+                        bad_word()
 
-
-
-
+        no_guess_left(HIDDEN_WORD)
+    
+    except:
+        fatal()
 
